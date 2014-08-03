32af209c1df18ad1e85ebde04d01888491d434b5
Ruben Rhodes <zoozoobaba@gmail.com>
2014-08-03 09:21:22 -0700

    convert {number =>letters} to {letter => number}

--- lib/legacy_data.rb
+++ lib/legacy_data.rb
@@ -1,14 +1,23 @@
-def legacy_data(letters, hash)
-  letter.each do | l |
-    letter = l
-  end
+def legacy_data(hash)
+  letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  converted_hash = {}
 
-  hash.each do |key, value|
-    if hash[key].index(letter) != nil
-      return key
+  letters.each do |letter|
+    hash.each do |key, value|
+      if hash[key].index(letter) != nil
+        converted_hash[letter] = key
+      end
     end
   end
-
+  puts converted_hash
+  return converted_hash
 end
 
-legacy_data(["A"], { 1  => ["A", "E", "I"]})
+
+legacy_data({ 1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2  => ["D", "G"],
+  3  => ["B", "C", "M", "P"],
+  4  => ["F", "H", "V", "W", "Y"],
+  5  => ["K"],
+  8  => ["J", "X"],
+  10 => ["Q", "Z"]})
